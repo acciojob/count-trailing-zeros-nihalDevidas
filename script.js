3669669c34f73c0807d06b29ceadfaebd8409a8c
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-12-07 17:59:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function trailingZeros(n) {
   //your JS code here. If required.
+
+	let zeros = 0;
+
+	let powOf5 = 5;
+
+	while(Math.floor(n/powOf5)>0){
+		zeros = zeros + Math.floor(n/powOf5)
+		powOf5 = powOf5*5;
+	}
+	return zeros;
 }
 
 const input = prompt("Enter a number");
